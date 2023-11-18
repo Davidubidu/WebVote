5841359a07ba6665f84f8a575084b6e8b30f7b51
TonMoshi <chingowamen@hotmail.com>
2023-11-18 20:44:24 +0100

    MongoDB connection done!

--- WebvoteBack/CreatePost/index.js
+++ WebvoteBack/CreatePost/index.js
@@ -3,36 +3,6 @@ module.exports = async function (context, req) {
     try{
 
         await testConection();
-        // if(!req.body){
-        //     context.res = {
-        //         status: 400,
-        //         body: "no request body, please pass a request body"
-        //     };
-        //     return;
-        // }
-
-        // const {name, roles, lobbies} = req.body;
-
-        // if(!name || !roles || !lobbies){
-        //     context.res = {
-        //         status: 400,
-        //         body: "name, roles or lobbies are Null, please pass all required parameters"
-        //     };
-        //     return;
-        // }
-
-        // const entity = {
-        //     PartitionKey: {'_': name},
-        //     RowKey: {'_':'1'},
-        //     roles: {'_': roles},
-        //     lobbies: {'_': lobbies}
-        // }
-        
-        // const result = await insertEntity("Users", entity);
-
-        // context.res = {
-        //     body: result
-        // };
 
     }catch(error){
         context.res = {
