1396c8edf6da740585ed47ad85c9c55b05b6754c
David <37439686+Davidubidu@users.noreply.github.com>
2023-11-21 10:35:10 +0100

    changes to service, functional insert user

--- WebvoteBack/CreatePost/index.js
+++ WebvoteBack/CreatePost/index.js
@@ -1,8 +1,25 @@
-const{testConection} = require("../services/tableService");
+const tableService = require("../services/tableService");
 module.exports = async function (context, req) {
     try{
+        if(!req.body){
+            context.res = {
+                status: 400,
+                body: "Please pass a request body"
+            };
+            return;
+        }
 
-        await testConection();
+        const {name, roles, password} = req.body;
+
+        if(!name || !roles || !password){
+            context.res = {
+                status: 400,
+                body: "name. roles or password are empty, please provide them all"
+            };
+            return;
+        }
+
+        await tableService.insertUser({name, roles, password});
 
     }catch(error){
         context.res = {
--- WebvoteBack/services/tableService.js
+++ WebvoteBack/services/tableService.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
 
+// Uniqueness for database and container
+const timeStamp = + new Date();
+// Set Database name and container name with unique timestamp
+const databaseName = `webvote`;
+const collectionName = `Users`;
 
-const testConection = async () => {
-    // Uniqueness for database and container
-    const timeStamp = + new Date();
-    // Set Database name and container name with unique timestamp
-    const databaseName = `webvote`;
-    const collectionName = `Users`;
+async function insertUser(user) {
 
     try {
         const client = new MongoClient(mongoConnectionString);
@@ -24,7 +24,9 @@ const testConection = async () => {
         const collection = db.collection(collectionName);
         console.log(`collection:\n${Object.keys(collection).map(key => `\t${key}: ${collection[key]}\n`)}`);
 
-        await collection.insertOne({name: "test", roles: "test", lobbies: "test"});
+        //construct an object to inject in the database matching the parameters expected in the database with the ones present in the object passed as argument
+        const userToInsert = {name: user.name, roles: user.roles, password: user.password}
+        await collection.insertOne(userToInsert);
 
         client.close()
 
@@ -33,5 +35,91 @@ const testConection = async () => {
     }  
 }
 
+async function getUser(userId) {
+    try {
+        const client = new MongoClient(mongoConnectionString);
 
-exports.testConection = testConection;
+        // connect to the server
+        await client.connect();
+
+        const options = client.options
+        console.log(`Options:\n${Object.keys(options).map(key => `\t${key}: ${options[key]}\n`)}`);
+
+        const db = client.db(databaseName);
+        console.log(`db:\n${Object.keys(db).map(key => `\t${key}: ${db[key]}\n`)}`);
+
+        const collection = db.collection(collectionName);
+        console.log(`collection:\n${Object.keys(collection).map(key => `\t${key}: ${collection[key]}\n`)}`);
+
+        //finds the user object corresponding with the filter object passed from the front
+        await collection.findOne(userId);
+
+        client.close()
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function deleteUser(userId){
+    try {
+        const client = new MongoClient(mongoConnectionString);
+
+        // connect to the server
+        await client.connect();
+
+        const options = client.options
+        console.log(`Options:\n${Object.keys(options).map(key => `\t${key}: ${options[key]}\n`)}`);
+
+        const db = client.db(databaseName);
+        console.log(`db:\n${Object.keys(db).map(key => `\t${key}: ${db[key]}\n`)}`);
+
+        const collection = db.collection(collectionName);
+        console.log(`collection:\n${Object.keys(collection).map(key => `\t${key}: ${collection[key]}\n`)}`);
+
+        //deletes the user corresponding with the filter object passed from the front
+        await collection.findOneAndDelete(userId);
+
+        client.close()
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function updateUser(userModification, userId){
+    try {
+        const client = new MongoClient(mongoConnectionString);
+
+        // connect to the server
+        await client.connect();
+
+        const options = client.options
+        console.log(`Options:\n${Object.keys(options).map(key => `\t${key}: ${options[key]}\n`)}`);
+
+        const db = client.db(databaseName);
+        console.log(`db:\n${Object.keys(db).map(key => `\t${key}: ${db[key]}\n`)}`);
+
+        const collection = db.collection(collectionName);
+        console.log(`collection:\n${Object.keys(collection).map(key => `\t${key}: ${collection[key]}\n`)}`);
+
+        //filter to finde the user to update, works only on Id
+        const filter = {'_id': userId}
+        //query with the changes to make in the user object
+        const updateQuery = {
+            $set: {userModification}
+        }
+        await collection.updateOne(updateQuery);
+
+        client.close()
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+module.exports = {insertUser, getUser, deleteUser, updateUser};
