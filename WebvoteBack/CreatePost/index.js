fd35d8b5da401519d01d932a4fb7574538606b7b
TonMoshi <chingowamen@hotmail.com>
2023-11-21 12:30:05 +0100

    feat(eslint): eslint and prettier

--- WebvoteBack/CreatePost/index.js
+++ WebvoteBack/CreatePost/index.js
@@ -1,47 +1,45 @@
-const tableService = require("../services/tableService");
-const {buildResponse, httpHandler} = require("../Middleware/afUtils");
+const tableService = require('../services/tableService');
+const { buildResponse, httpHandler } = require('../Middleware/afUtils');
 
 module.exports = async function (context) {
-    const handlers = [
-        { method: 'get', function: handleGet }, 
-        { method: 'post', function: handlePost }
-    ];
-    await httpHandler(context, handlers);
-}
+  const handlers = [
+    { method: 'get', function: handleGet },
+    { method: 'post', function: handlePost }
+  ];
+  await httpHandler(context, handlers);
+};
 
 async function handleGet(context) {
-
-    // Logic here
+  // Logic here
 }
 
 async function handlePost(context) {
-    const req = context.req;
+  const req = context.req;
 
-    try{
-        if(!req.body){
-            context.res = {
-                status: 400,
-                body: "Please pass a request body"
-            };
-            return;
-        }
-
-        const {name, roles, password} = req.body;
-
-        if(!name || !roles || !password){
-            context.res = {
-                status: 400,
-                body: "name. roles or password are empty, please provide them all"
-            };
-            return;
-        }
-
-        await tableService.insertUser({name, roles, password});
-
-    }catch(error){
-        context.res = {
-            status: 500, 
-            body: error.message,       
-        };
+  try {
+    if (!req.body) {
+      context.res = {
+        status: 400,
+        body: 'Please pass a request body'
+      };
+      return;
     }
+
+    const { name, roles, password } = req.body;
+
+    if (!name || !roles || !password) {
+      context.res = {
+        status: 400,
+        body: 'name. roles or password are empty, please provide them all'
+      };
+      return;
+    }
+
+    await tableService.insertUser({ name, roles, password });
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    };
+  }
 }
